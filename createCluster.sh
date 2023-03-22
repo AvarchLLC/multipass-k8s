709a8ae5980403a2b15d9f355e6e0aeb7d89e9c2
Manish Ranjan <ranjanmanishr@gmail.com>
2023-03-22 14:53:00 -0400

    updated calico yaml url

--- createCluster.sh
+++ createCluster.sh
@@ -86,7 +86,7 @@ EOF
 EOF
 
  multipass exec master -- bash <<EOF
-    kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
+    kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
 EOF
 
 
